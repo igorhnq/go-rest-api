ca8a846dac519b5f234ebb1497bfa362e52d9967
igorhnq <igorhenrique102@gmail.com>
2025-07-13 13:48:48 -0300

    feat: add database connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,36 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"go-rest-api/controllers"
+	"go-rest-api/db"
+	"go-rest-api/repositories"
+	"go-rest-api/usecases"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 
 	server := gin.Default()
+
+	dbConnection, err := db.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+
+	ProductRepository := repositories.NewProductRepository(dbConnection)
+
+	ProductUseCase := usecases.NewProductUseCase(ProductRepository)
+
+	ProductControler := controllers.NewProductController(ProductUseCase)
 
 	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	server.GET("/products", ProductControler.GetProducts)
 
 	server.Run(":8080")
 }
